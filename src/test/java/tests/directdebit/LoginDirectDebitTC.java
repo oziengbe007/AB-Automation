aeea6eb826a0413002a7528a03736272ec451fe9
ragholor <ragholor@LHQIT24A004.abnigeria.com>
2024-10-24 17:05:26 +0100

    commit for the day

--- src/test/java/tests/directdebit/LoginDirectDebitTC.java
+++ src/test/java/tests/directdebit/LoginDirectDebitTC.java
@@ -1,5 +1,17 @@
 package tests.directdebit;
 
-public class LoginDirectDebitTC {
+import org.testng.annotations.Test;
+
+import base.BaseClass;
+import pages.directdebit.DirectDebitLoginPage;
+
+public class LoginDirectDebitTC extends BaseClass {
 	
+	@Test(dataProvider = "loginData")
+	public void verifyLogin(String username, String password, boolean isValid)
+	{
+		DirectDebitLoginPage loginPage = new DirectDebitLoginPage(driver);
+		loginPage.login(username, password);
+		if
+	}
 }
